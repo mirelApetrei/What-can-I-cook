80ab7318929914a684ef1890b1fd8e5fbb726279
mirel.apetrei <mirel_apetrei@icloud.com>
2023-05-17 10:24:47 +0200

    RecipesBinding.kt -> modified --> renewed <--

--- app/src/main/java/com/example/whatcanicook/bindingadapters/RecipesBinding.kt
+++ app/src/main/java/com/example/whatcanicook/bindingadapters/RecipesBinding.kt
@@ -3,59 +3,45 @@ package com.example.whatcanicook.bindingadapters
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
-import coil.load
-import com.example.whatcanicook.R
-
+import com.example.whatcanicook.data.database.RecipesEntity
+import com.example.whatcanicook.models.FoodRecipe
+import com.example.whatcanicook.utils.NetworkResult
 
 class RecipesBinding {
 
     companion object {
 
-        @BindingAdapter("loadImageFromUrl")
+        @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
         @JvmStatic
-        fun loadImageFromUrl(imageView: ImageView, imageUrl: String) {
-            imageView.load(imageUrl) {
-                crossfade(600)
-                error(R.drawable.ic_error_placeholder)
+        fun errorImageViewVisibility(
+            imageView: ImageView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ) {
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                imageView.visibility = View.VISIBLE
+            } else if (apiResponse is NetworkResult.Loading) {
+                imageView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                imageView.visibility = View.INVISIBLE
             }
         }
 
-        @BindingAdapter("setNumberOfLikes")
+        @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
         @JvmStatic
-        fun setNumberOfLikes(textView: TextView, likes: Int){
-            textView.text = likes.toString()
-        }
-
-        @BindingAdapter("setNumberOfMinutes")
-        @JvmStatic
-        fun setNumberOfMinutes(textView: TextView, minutes: Int){
-            textView.text = minutes.toString()
-        }
-
-        @BindingAdapter("applyVeganColor")
-        @JvmStatic
-        fun applyVeganColor(view: View, vegan: Boolean) {
-            if(vegan){
-                when(view){
-                    is TextView -> {
-                        view.setTextColor(
-                            ContextCompat.getColor(
-                                view.context,
-                                R.color.green
-                            )
-                        )
-                    }
-                    is ImageView -> {
-                        view.setColorFilter(
-                            ContextCompat.getColor(
-                                view.context,
-                                R.color.green
-                            )
-                        )
-                    }
-                }
+        fun errorTextViewVisibility(
+            textView: TextView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ) {
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                textView.visibility = View.VISIBLE
+                textView.text = apiResponse.message.toString()
+            } else if (apiResponse is NetworkResult.Loading) {
+                textView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                textView.visibility = View.INVISIBLE
             }
         }
 
