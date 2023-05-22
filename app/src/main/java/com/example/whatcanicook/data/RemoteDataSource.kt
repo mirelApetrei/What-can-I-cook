288ebe5fa30e844ebd43f90d0dde07c2e4fed92a
mirel.apetrei <mirel_apetrei@icloud.com>
2023-05-22 21:45:40 +0200

    Search function initiated, and some comments

--- app/src/main/java/com/example/whatcanicook/data/RemoteDataSource.kt
+++ app/src/main/java/com/example/whatcanicook/data/RemoteDataSource.kt
@@ -6,12 +6,19 @@ import com.example.whatcanicook.models.FoodRecipe
 import retrofit2.Response
 import javax.inject.Inject
 
+// This class is a remote data source for food recipes.
 class RemoteDataSource @Inject constructor(
-    private val foodRecipesApi: FoodRecipesApi
+    private val foodRecipesApi: FoodRecipesApi          //<--  The `foodRecipesApi` property is an `Api` object that provides access to the food recipes API.
 ) {
 
+    // This suspend function gets recipes from the server
     suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
         return foodRecipesApi.getRecipes(queries)
     }
 
+
+    // This suspend function searches for recipes from the server.
+    suspend fun searchRecipes(searchQuery: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.searchRecipes(searchQuery)
+    }
 }
